3324d71c4a15d692dec28ea709cc5c64fff40d36
smolly71 <sven.smolders@ictmbo.nl>
2020-06-18 22:44:57 +0200

    deleten van route

--- src/Routes.js
+++ src/Routes.js
@@ -51,15 +51,31 @@ class Routes extends Component {
     this.props.onRouteSelect(data.json);
   }
 
+  deleteRoute = (route_id) => {
+    console.log(route_id);
+    fetch('https://wandelappbackend-v5.herokuapp.com/route?id='+ route_id +'&cuid=test',{
+      method: 'DELETE'
+    })
+     .then(res => res.text()) // or res.json()
+      .then(res => console.log(res));
+    window.location.reload();
+  };
+
   render() {
     const style = {
       flex: '1 1 0'
     };
+    const verwijder = {
+      color: 'red',
+      cursor: 'pointer',
+    };
+
     return (
       <div style={style}>
+        <h2>Routes</h2><Alert/>
         <ul>
           {this.state.routes.map(route =>
-            <li onClick={this.selectRoute.bind(this, route.data)} key={route.data._id}>{route.data.json.features[0].properties.name}</li>
+            <li onClick={this.selectRoute.bind(this, route.data)} key={route.data._id}>{route.data.json.features[0].properties.name} <p style={verwijder} onClick={this.deleteRoute.bind(this, route.data._id)}>verwijder route</p></li>
           )}
         </ul>
       </div>
